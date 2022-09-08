81bba48bd8bb1a248c52ad6a0a09ca69a9f449ca
deehrv <deehrv01@gmail.com>
2022-09-08 13:07:50 -0300

    progamaDeSaude

---
+++ saude.js
@@ -0,0 +1,14 @@
+ let nome,altura,peso,datadeInternacao,diabetico
+  
+ nome =prompt ("escreva o seu nome: ");
+
+ peso =prompt ("escreva o seu peso: ");
+ 
+ 
+ altura  =prompt ("escreva a sua altura: " );
+  
+ diabetico= prompt(" voce é diabetico? ")
+ 
+ 
+  
+alert  ("seu nome: "+nome +"\n"+ "seu peso é: " +peso +"\n"+"sua altura é "+altura +"\n" +"diabetico: "+ diabetico)
